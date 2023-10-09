bd19552fcedc34afb004f93a531a5fc498a265bf
Jonatas Brito <jonatas.brito.ferreira@gmail.com>
2023-10-09 09:51:07 -0300

    style: add css files

--- quix-alert/src/components/layout/NavBar/NavBar.tsx
+++ quix-alert/src/components/layout/NavBar/NavBar.tsx
@@ -1,58 +1,38 @@
 import React from "react"
 
-import {Layout, Input, Menu} from "antd"
-import {styled} from "styled-components";
 import DropDown from "../DropDown/DropDown";
+import NavBarItems from "../NavBarItems/NavBarItems";
 
 import quixalertLogo from "../../../assets/images/quixalert_logo.png"
-import { MessageFilled, BellFilled, ClockCircleFilled } from "@ant-design/icons";
-import Icon from "@ant-design/icons/lib/components/Icon";
 
-function NavBar(){
+import { Layout, Input } from "antd"
+
+import "../NavBar/styles.css"
+
+function NavBar() {
   const { Header } = Layout;
   const { Search } = Input
 
-  const LogoAndSearchBar = styled.div`
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    gap: 10px;
-  `
-  const SearchStyle = styled.div`
-    display: flex;
-  `
-
   return (
-    <Layout className="navBar">
-      <Header style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: "#25252D", padding: 0}}>
-        <LogoAndSearchBar>
-          <div className="logo" style={{ color: 'white' }}>
-            <img src={quixalertLogo} style={{width: "38px", height: "34px", marginTop: "25px", marginLeft: "20px"}} alt="QuixAlert Logo"/>
+    <Layout className="navbar">
+      <Header className="navbar-header">
+        <div className="logo-search-bar">
+          <div className="navbar-container">
+            <img className="navbar-logo" src={quixalertLogo} alt="QuixAlert Logo"/>
           </div>
 
-          <SearchStyle>
-            <Search placeholder="Pesquisar" style={{marginLeft: "30px", width: '473px' }} />
-          </SearchStyle>
-        </LogoAndSearchBar>
-
-        <div style={{display: "flex", justifyContent: "center", alignItems:"center"}}>
-          <DropDown/>
-
-          <div style={{display: "flex", height: "64px"}}>
-            <div style={{width: "65px", display: "flex", justifyContent: "center", borderLeft: "1px solid #1D1E23", borderRight: "1px solid #1D1E23"}}>
-              <BellFilled style={{fontSize: "17px", color: "white"}}/>
-            </div>
-            <div style={{width: "65px", display: "flex", justifyContent: "center", borderRight: "1px solid #1D1E23"}}>
-              <MessageFilled style={{fontSize: "17px", color: "white"}}/>
-            </div>
-            <div style={{width: "65px", display: "flex", justifyContent: "center"}}>
-              <ClockCircleFilled style={{fontSize: "17px", color: "white"}}/>
-            </div>
+          <div className="search-bar-container">
+            <Search className="search-bar" placeholder="Pesquisar"/>
           </div>
+        </div>
+
+        <div className="navbar-menu">
+          <DropDown />
+          <NavBarItems />
         </div>
       </Header>
     </Layout>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;

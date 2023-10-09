cff710f304ea5ed56a6fc3fa471d9ff56ed12451
Jonatas Brito <jonatas.brito.ferreira@gmail.com>
2023-10-09 12:51:09 -0300

    style: reorganize navbar css files

--- quix-alert/src/components/layout/DropDown/DropDown.tsx
+++ quix-alert/src/components/layout/DropDown/DropDown.tsx
@@ -1,33 +1,32 @@
 import React, {useState} from "react"
+
 import DropDownItem from "../DropDownItem/DropDownItem";
-import {DropDownMenuItems} from "../../../styles";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+
 import userImg from "../../../assets/images/user.jpeg"
+
 import {DownOutlined, FormOutlined, LogoutOutlined, UserOutlined} from "@ant-design/icons";
 
-
+import "./styles.css"
 
 function DropDown(){
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   return (
     <div>
-      <div style={{display:"flex", justifyContent:"space-between", alignItems:"center", gap: "15px"}}>
-        <img style={{width:'50px', borderRadius:'50%', border: "1px solid white"}} src={userImg}/>
-        <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
-          <div style={{color: "white", fontWeight: "bold", marginBottom: "5px", lineHeight: 1}}>João Victor</div>
-          <div style={{color: "white", fontSize: 10, lineHeight: 1}}>Administrador</div>
+      <div className="user-menu" >
+        <img className="user-image" src={userImg}/>
+        <div className="user-details">
+          <div className="user-name" >Phablo Gabriel</div>
+          <div className="user-role" >Administrador</div>
         </div>
-          <DownOutlined onClick={() => setOpen(!open)} style={{color: "white", marginRight: "5px" }}/>
+
+        <DownOutlined className="arrow-item" onClick={() => setOpen(!open)} />
       </div>
-      <DropDownMenuItems className={`${open? 'active' : 'inactive'}`}>
-        <ul style={{listStyleType:"none", padding:20}}>
-          <DropDownItem icon={<UserOutlined />} text="Meu perfil"/>
-          <DropDownItem icon={<FormOutlined />} text="Atendidos"/>
-          <DropDownItem icon={<LogoutOutlined />} text="Sair"/>
-        </ul>
-      </DropDownMenuItems>
+      <ul className={`dropdown-container ${open? ' active' : ' inactive'}`} >
+        <DropDownItem icon={<UserOutlined />} text="Meu perfil"/>
+        <DropDownItem icon={<FormOutlined />} text="Atendidos"/>
+        <DropDownItem icon={<LogoutOutlined />} text="Sair"/>
+      </ul>
     </div>
   )
 }
--- quix-alert/src/components/layout/DropDownItem/DropDownItem.tsx
+++ quix-alert/src/components/layout/DropDownItem/DropDownItem.tsx
@@ -1,17 +1,16 @@
-import React from "react"
-import Icon, {ArrowDownOutlined} from "@ant-design/icons";
-import {Card} from "antd";
-import DropDownItemModel from '../../../models/DropDownItemModel'
+import React from "react";
+
+import DropDownItemModel from '../../../models/DropDownItemModel';
+
+import "./styles.css";
 
 function DropDownItem(props: DropDownItemModel){
   return (
-  <li className="dropDownItem" style={{padding:'0px'}}>
-    <div style={{display:"flex", justifyContent:"start"}}>
-      {props.icon}
-      <a style={{marginLeft:10}}>{props.text}</a>
-    </div>
-  </li>
-  )
+    <li className="dropdown-item">
+        {props.icon}
+        <a>{props.text}</a>
+    </li>
+  );
 }
 
 

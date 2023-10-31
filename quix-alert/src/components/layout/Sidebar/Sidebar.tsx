45342a131cc08bdf904e3b100bace8a8eba301a3
Jonatas Brito <jonatas.brito.ferreira@gmail.com>
2023-10-31 20:43:52 -0300

    chore: add router configuration

--- quix-alert/src/components/layout/Sidebar/Sidebar.tsx
+++ quix-alert/src/components/layout/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate, useLocation, Link  } from 'react-router-dom';
 
 import home from "../../../assets/icons/home.svg";
 import alert from "../../../assets/icons/alert.svg";
@@ -11,17 +12,44 @@ import person from "../../../assets/icons/person.svg";
 import "./styles.css"
 
 function Sidebar () {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const isHomePage = location.pathname === "/home";
+  const isAdoptionPage = location.pathname === "/adoption";
+  const isUsersPage = location.pathname === "/user";
+
   return (
     <>
       <div className="sidebar-container">
         <div className="sidebar-content-container">
-          <img src={home} className="sidebar-icon"/>
-          <img src={person} className="sidebar-icon"/>
-          <img src={document} className="sidebar-icon"/>
-          <img src={dog} className="sidebar-icon"/>
-          <img src={alert} className="sidebar-icon"/>
-          <img src={gear} className="sidebar-icon"/>
-          <img src={question_mark} className="sidebar-icon"/>
+          <a onClick={() => {navigate("/home")}}>
+            <img src={home} className={`sidebar-icon${isHomePage ? '-active' : ''}`}/>
+          </a>
+
+          <a onClick={() => {navigate("/home")}}>
+            <img src={person} className="sidebar-icon"/>
+          </a>
+
+          <a onClick={() => {navigate("/home")}}>
+            <img src={document} className="sidebar-icon"/>
+          </a>
+
+          <a onClick={() => {navigate("/adoption")}}>
+            <img src={dog} className={`sidebar-icon${isAdoptionPage ? '-active' : ''}`}/>
+          </a>
+          
+          <a onClick={() => {navigate("/home")}}>
+            <img src={alert} className="sidebar-icon"/>
+          </a>
+
+          <a onClick={() => {navigate("/home")}}>
+            <img src={gear} className="sidebar-icon"/>
+          </a>
+
+          <a onClick={() => {navigate("/home")}}>
+            <img src={question_mark} className="sidebar-icon"/>
+          </a>
         </div>
       </div>
       <div className="sidebar-detail">

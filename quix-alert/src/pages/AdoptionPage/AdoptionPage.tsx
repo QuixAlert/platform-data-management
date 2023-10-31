7e172bb989f3120c4283f5f56cd9af2dbb35d0ff
Jonatas Brito <jonatas.brito.ferreira@gmail.com>
2023-10-31 16:16:06 -0300

    fix: change pages folders

---
+++ quix-alert/src/pages/AdoptionPage/AdoptionPage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import PagePanel from "../../components/layout/PagePanel/PagePanel"
+import AnimalContainer from "../../components/layout/AnimalDetailContainer/AnimalDetailContainer";
+import PersonInformationContainer from "../../components/layout/PersonInformationContainer/PersonInformationContainer";
+import ApprovalContainer from "../../components/layout/ApprovalContainer/ApprovalContainer";
+
+import "./styles.css"
+
+function AdoptionPage() {
+  return(
+      <PagePanel>
+        <div className="adoption-container">
+          <PersonInformationContainer />
+          <AnimalContainer />
+          <ApprovalContainer />
+        </div>
+      </PagePanel>
+  );
+}
+
+export default AdoptionPage;

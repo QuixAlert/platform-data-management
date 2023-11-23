222d1a795aa07ee675d6e022d87488f47d07d4c7
Jonatas Brito <jonatas.brito.ferreira@gmail.com>
2023-11-23 19:47:51 -0300

    chore: add adoption cards interfaces and props

--- quix-alert/src/components/layout/AdoptionCard/AdoptionCard.tsx
+++ quix-alert/src/components/layout/AdoptionCard/AdoptionCard.tsx
@@ -2,48 +2,50 @@ import React from "react";
 
 import { useNavigate } from "react-router";
 
-import requester from "../../../assets/images/requester.jpeg";
-import responsible from "../../../assets/images/responsible.jpeg";
-import animal from "../../../assets/images/dog2.jpeg";
+import AdoptionCard from "../../../interfaces/AdoptionCard";
+
+import requesterPicture from "../../../assets/images/requester.jpeg";
+import responsiblePicture from "../../../assets/images/responsible.jpeg";
+import animalPicture from "../../../assets/images/dog2.jpeg";
 import dog from "../../../assets/icons/dog.svg"
 import male from "../../../assets/icons/male.svg"
 
 import "./style.css"
 
-function AdoptionCard() {
+function AdoptionCard(Props: AdoptionCard) {  
   const navigate = useNavigate();
 
   return (
     <div className="adoption-card" title="">
       <div className="card-left">
         <div className="card-person-container">
-          <img className="card-person-photo" src={ requester } alt="person-photo" />
+          <img className="card-person-photo" src={ requesterPicture } alt="person-photo" />
           <div className="card-person-role-and-name">
             <p className="card-person-role">Solicitante:</p>
-            <p className="card-person-name">Thiago Maia</p>
+            <p className="card-person-name">{ Props.requesterName }</p>
           </div>
         </div>
         <div className="card-adoption-info-grid">
           <div className="card-adoption-info-line">
             <h3>Tipo de solicitação:</h3>
-            <span className="card-solicitation-type">Adoção</span>
+            <span className="card-solicitation-type">{ Props.solicitationType }</span>
           </div>
           <div className="card-adoption-info-line">
             <h3>Registro:</h3>            
-            <p>3432121-78</p>
+            <p>{ Props.registerNumber }</p>
           </div>
           <div className="card-adoption-info-line">
             <h3>Data da solicitação:</h3>            
-            <p>31/08/2023</p>
+            <p>{ Props.solicitationDate }</p>
           </div>
         </div>
         <div className="card-adoption-animal-info">
           <div className="card-animal-photo-container">
             <h3>Animal Solicitado:</h3>
-            <img className="card-animal-photo" src={ animal } alt="animal image"/>
+            <img className="card-animal-photo" src={ animalPicture } alt="animal image"/>
           </div>
           <div className="card-animal-sub-info">
-            <h3>Sheldon</h3>
+            <h3>{ Props.animalName }</h3>
             <div className="card-animal-icons">
               <img src={dog} alt="dog icon"/>
               <img src={male} alt="dog icon"/>
@@ -54,24 +56,24 @@ function AdoptionCard() {
 
       <div className="card-right">
         <div className="card-person-container">
-          <img className="card-person-photo" src={ responsible } alt="person-photo" />
+          <img className="card-person-photo" src={ responsiblePicture } alt="person-photo" />
           <div className="card-person-role-and-name">
             <p className="card-person-role">Responsável:</p>
-            <p className="card-person-name">Samuel Honorato</p>
+            <p className="card-person-name">{ Props.responsibleName }</p>
           </div>
         </div>
         <div className="card-status-info">
         <div className="card-info-line">
             <h2>Dias em aberto:</h2>
-            <p>Está com 20 dias</p>
+            <p>{ Props.openDays }</p>
           </div>
           <div className="card-info-line">
             <h2>Status:</h2>
-            <p>Esperando Assinatura</p>
+            <p>{ Props.status }</p>
           </div>
           <div className="card-info-line">
             <h2>Conclusão Prevista:</h2>
-            <p>05/09/2023</p>
+            <p>{ Props.expectedDate }</p>
           </div>
         </div>
         <div className="card-see-more">
---
+++ quix-alert/src/interfaces/AdoptionCard.ts
@@ -0,0 +1,18 @@
+interface AdoptionCard {
+  requesterName: string,
+  requesterPicturePath: string,
+  responsibleName: string,
+  responsiblePicturePath: string,    
+  solicitationType: string,
+  registerNumber: string,
+  solicitationDate: string,
+  animalPicturePath: string,
+  animalName: string,
+  animalGender: string,
+  animalType: string,
+  openDays: string,
+  status: string,
+  expectedDate: string,
+}
+
+export default AdoptionCard;

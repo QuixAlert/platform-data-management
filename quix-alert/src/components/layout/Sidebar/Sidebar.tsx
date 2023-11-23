eb614c052f7dbb39312146cc94bf8d1bc3e8f7f1
Jonatas Brito <jonatas.brito.ferreira@gmail.com>
2023-11-23 09:56:27 -0300

    style: add sidebar color toggle

--- quix-alert/src/components/layout/Sidebar/Sidebar.tsx
+++ quix-alert/src/components/layout/Sidebar/Sidebar.tsx
@@ -10,6 +10,13 @@ import question_mark from "../../../assets/icons/question_mark.svg";
 import document from "../../../assets/icons/document.svg";
 import person from "../../../assets/icons/person.svg";
 
+import { IoHome, IoPersonSharp } from "react-icons/io5";
+import { MdOutlinePets } from "react-icons/md";
+import { FaFileAlt } from "react-icons/fa";
+import { GoAlertFill } from "react-icons/go";
+import { FaGear } from "react-icons/fa6";
+import { BsFillQuestionCircleFill } from "react-icons/bs";
+
 import "./styles.css"
 
 function Sidebar () {
@@ -21,38 +28,39 @@ function Sidebar () {
   const isUsersPage = location.pathname === "/user";
   const isConfigPage = location.pathname === '/config';
   const isHelpPage = location.pathname === '/help';
-
+  const isReportPage = location.pathname === '/report';
+  
   return (
     <>
       <div className="sidebar-container">
         <div className="sidebar-content-container">
 
           <a onClick={() => {navigate("/home")}}>
-            <img src={home} className={`sidebar-icon`}/>
+            <IoHome className={`sidebar-icon ${ isHomePage ? 'active' : '' }`} />
           </a>
 
           <a onClick={() => {navigate("/home")}}>
-            <img src={person} className="sidebar-icon"/>
+            <IoPersonSharp className={`sidebar-icon ${ isUsersPage ? 'active' : '' }`}/>
           </a>
 
           <a onClick={() => {navigate("/home")}}>
-            <img src={document} className="sidebar-icon"/>
+            <FaFileAlt className={`sidebar-icon`}/>
           </a>
 
           <a onClick={() => {navigate("/adoption")}}>
-            <img src={dog} className={`sidebar-icon`}/>
+            <MdOutlinePets className={`sidebar-icon ${ isAdoptionPage ? 'active' : '' }`}/>
           </a>
           
           <a onClick={() => {navigate("/report")}}>
-            <img src={alert} className="sidebar-icon"/>
+            <GoAlertFill className={`sidebar-icon ${ isReportPage ? 'active' : '' }`}/>
           </a>
 
           <a onClick={() => {navigate("/config")}}>
-            <img src={gear} className="sidebar-icon"/>
+            <FaGear className={`sidebar-icon ${ isConfigPage ? 'active' : '' }`}/>
           </a>
 
           <a onClick={() => {navigate("/help")}}>
-            <img src={question_mark} className="sidebar-icon"/>
+            <BsFillQuestionCircleFill className={`sidebar-icon ${ isHelpPage ? 'active' : '' }`}/>
           </a>
           
         </div>

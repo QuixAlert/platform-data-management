7062c1e5f7e17201051cc4e031edcbdf3d643a63
João Victor Carvalho <joaocarvoli.eng@alu.ufc.br>
2023-11-24 14:33:42 -0300

    Update firebaseUsers.ts

--- quix-alert/src/api/firebaseUsers.ts
+++ quix-alert/src/api/firebaseUsers.ts
@@ -1,29 +1,29 @@
 import {AuthTokens} from "./model/AuthTokens";
 import {api} from "./config/axiosBaseConfig";
 import {ADOPTIONS_PATH, ANIMALS_PATH, FIREBASE_USERS_PATH} from "../consts";
-import {Animal} from "./model/Animal";
+import {FirebaseUser} from "./model/FirebaseUser";
 
-export async function getAllFirebaseAppUsers(token: AuthTokens): Promise<Adoption[]>{
+export async function getAllFirebaseAppUsers(token: AuthTokens): Promise<FirebaseUser[]>{
     try {
         const { data, status } = await api.get(`${FIREBASE_USERS_PATH}`, {
             headers: { Authorization: `Bearer ${token.token}` }
         })
 
         if(status == 200){
-            return data as Adoption[]
+            return data as FirebaseUser[]
         }
     } catch (error){
         throw new Error("Login error!")
     }
 }
 
-export async function getFirebaseAppUsersId(token: AuthTokens, userId: string): Promise<Adoption[]>{
+export async function getFirebaseAppUserById(token: AuthTokens, userId: string): Promise<FirebaseUser>{
     try {
         const { data, status } = await api.get(`${FIREBASE_USERS_PATH}/${userId}`, {
             headers: { Authorization: `Bearer ${token.token}` }
         })
         if(status == 201){
-            return data as Adoption[]
+            return data as FirebaseUser
         }
     } catch (error){
         throw new Error("Login error!")
